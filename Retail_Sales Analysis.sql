eb9bcf3480721ca81e8621133cf577bf19bcdb64
neha sharma <Sharmaneha2073@gmail.com>
2025-01-02 15:37:16 +0545

    sql

---
+++ Retail_Sales Analysis.sql
@@ -0,0 +1,74 @@
+-- CREATE DATABASE sql_project;
+
+-- CREATE TABLE retail_sales(
+-- transaction_id int primary KEY,
+-- sale_date DATE,
+-- sale_time TIME,
+-- customer_id int,
+-- gender varchar(15),
+-- age int,
+-- category varchar(25),
+-- quantity int,
+-- price_per_unit float,
+-- cogs float,
+-- total_sale float
+-- );
+
+-- select * from retail_sales;
+
+-- select COUNT(*) from retail_sales;
+
+-- ALTER TABLE retail_sales
+-- CHANGE ï»¿transactions_id transactions_id int;
+
+
+-- ALTER TABLE retail_Sales
+-- CHANGE quantiy quantity int;
+
+
+-- select * from retail_Sales
+--  WHERE 
+--  customer_id IS NULL
+--  or
+--  transactions_id IS NULL
+--  OR
+--   sale_date IS NULL
+--   or
+--    sale_time IS NULL
+--    or
+--    gender IS NULL
+--    or
+--    age IS NULL
+--    or
+--    category IS NULL
+--    or
+--    quantity IS NULL
+--    or 
+--    price_per_unit IS NULL
+--   or
+--   cogs IS NULL
+--   or
+--   total_sale IS NULL;
+
+-- DELETE FROM retail_sales
+-- WHERE customer_id IS NULL 
+-- or transactions_id IS NULL
+-- OR sale_date IS NULL
+-- or sale_time IS NULL
+-- or gender IS NULL
+-- or age IS NULL
+-- or category IS NULL
+-- or quantity IS NULL
+-- or price_per_unit IS NULL
+-- or cogs IS NULL
+-- or total_sale IS NULL;
+
+
+
+
+
+
+
+
+
+
